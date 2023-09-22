342f9b4cc301a3d57495f69654ba909780b1d66b
liamcal <coolboyrocx@gmail.com>
2023-09-22 14:42:27 +1000

    Undo unrelated change

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,20 +1,20 @@
-import { useEffect } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { lazyWithPreload } from 'react-lazy-with-preload';
 import { Fab } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import MacroList from './MacroList';
+import { useEffect } from 'react';
 
 const CreateMacro = lazyWithPreload(() => import('./CreateMacro'));
 const ViewMacro = lazyWithPreload(() => import('./ViewMacro'));
 const EditMacro = lazyWithPreload(() => import('./EditMacro'));
 
 const Home = () => {
-    useEffect(() => {
-        CreateMacro.preload();
-        ViewMacro.preload();
-        EditMacro.preload();
-    }, []);
+    // useEffect(() => {
+    //     CreateMacro.preload();
+    //     ViewMacro.preload();
+    //     EditMacro.preload();
+    // }, []);
 
     return (
         <div>
